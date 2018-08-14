3d243e3126c2e863952c6235869910ae544edd07
Alexeis <panthon.six@gmail.com>
2018-08-14 10:10:23 -0400

    Cliente

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'tcc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'typescript-chat-client';
+  ngOnInit(): void {
+  }
+  private initModel(): void {
+  }
+}
